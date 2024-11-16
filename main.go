f7814c5bfbe65ebe0ca1316d696937091844fb5e
Burak Ataş <batas219@gmail.com>
2024-11-16 15:49:35 +0300

    middleware not eacces

--- main.go
+++ main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"github.com/Burak-Atas/kahve_fali/controller"
-	"github.com/Burak-Atas/kahve_fali/middleware"
 	"github.com/Burak-Atas/kahve_fali/openai"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
-	model := openai.NewOpenAI("")
+	model := openai.NewOpenAI("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")
 	model.NewChat("")
 	routers := gin.New(func(e *gin.Engine) {})
 
@@ -18,7 +17,7 @@ func main() {
 	routers.POST("/login", controller.SignIn())
 	routers.POST("/signup", controller.SignUp())
 
-	routers.Use(middleware.AuthMiddleware())
+	// routers.Use(middleware.AuthMiddleware())
 
 	groupV1 := routers.Group("/v1")
 	groupV1.POST("/fortunetelling", controller.Fortunetelling(model))
